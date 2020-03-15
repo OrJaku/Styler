5c2075fd5071eaa238a97ee69d141e4bd8604f63
OrJaku <kuba.orzel@hotmail.com>
2020-03-15 22:58:56 +0100

    New scriping script

--- app.py
+++ app.py
@@ -1,1 +1,38 @@
-import BeautifulSoup
+from bs4 import BeautifulSoup
+import json
+import requests
+
+
+url = ''
+with open('links.json') as f:
+    data = json.load(f)
+    for u in data:
+        try:
+            u = u['reserved']
+        except TypeError:
+            u = None
+        except KeyError:
+            u = None
+        if u:
+            print(u)
+            url = u
+
+
+result = requests.get(url)
+
+src = result.content
+soup = BeautifulSoup(src, 'lxml')
+links = soup.find_all("a")
+for link in links:
+    try:
+        category = str(link).split("/")[-5]
+    except IndexError:
+        category = None
+    print(category)
+
+# i = 0
+# for link in links:
+#     i += 1
+#     name = str(link).split('"', 2)[1]
+#     print(name)
+# print(i)
